4c686a15749756325de0406340490c80136511bb
Danish Zaman Khan <danishzamankhan471996@gmail.com>
2022-01-31 22:24:54 +0530

    Added POST,PUT,PATCH and DELETE methods to our API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,10 +3,17 @@
 
 from rest_framework.views import APIView
 from rest_framework.views import Response
-
+#To get the status of response if the response is bad
+from rest_framework import status   
+#importing the serializer we created 
+from profiles_api import serializer
 
 class HelloApiView(APIView):
     ''' Test API ''' 
+    
+    #Configures our API view with the serializer we created 
+    serializer_class = serializer.HelloSerializer 
+        
     def get(self, request, format = None):
         '''Returns list of API views features'''
         an_apiview = [
@@ -23,4 +30,32 @@ class HelloApiView(APIView):
             'an_apiview' : an_apiview
             }
                         )
-
+    
+    def post(self, request):
+        
+        '''Create hello massage with out name '''
+        
+        serializer = self.serializer_class(data = request.data)
+        
+        #Django framework provides the functionality to validate
+        if serializer.is_valid():
+            #We can retrieve any field we define in serializer this way like name field
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'  #f string functionality to insert name in the string 
+            return Response({'message': message})
+        else:
+            #respones by default return http_200_okay
+            return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+        
+    def put(self, request, pk = None):
+        #pk is for the id we are updating
+        '''Handle updating an object'''
+        return Response({'message':'PUT'})
+    
+    def patch(self, request, pk = None):
+        '''Handle partial update of an object'''
+        return Response({'message':'PATCH'})
+    
+    def delete(self, request, pk= None):
+        '''Delete an object'''
+        return Response({'message':'Delete'})
